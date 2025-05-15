d9af04f82184701ffd30d5fcf0860070dcced030
KappaElite <rozkiewiczmilosz@gmail.com>
2025-05-16 00:01:10 +0200

    Feat(Socket) Basic implementation of sockets in backend.

---
+++ Backend/src/main/java/com/example/ztpai/controller/WebSocketChatController.java
@@ -0,0 +1,36 @@
+package com.example.ztpai.controller;
+
+import com.example.ztpai.DTO.MessageDTO;
+import com.example.ztpai.repository.UserRepository;
+import com.example.ztpai.service.MessageService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+@Controller
+public class WebSocketChatController {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final MessageService messageService;
+    private final UserRepository userRepository;
+
+    public WebSocketChatController(SimpMessagingTemplate messagingTemplate, MessageService messageService, UserRepository userRepository) {
+        this.messagingTemplate = messagingTemplate;
+        this.messageService = messageService;
+        this.userRepository = userRepository;
+    }
+
+    @MessageMapping("/send")
+    public void processMessage(@Payload MessageDTO message, Principal principal) {
+        String sender = principal.getName();
+        Long senderId= userRepository.findByUsername(sender).get().getId();
+        messageService.AddMesage(message.getContent(), senderId, message.getReceiver().getId());
+        messagingTemplate.convertAndSendToUser(
+                message.getReceiver().getUsername(),
+                "/queue/messages",
+                message
+        );
+    }
+}
