c5d39631ae691637e88cf3960101c44ba573bd12
KappaElite <rozkiewiczmilosz@gmail.com>
2025-05-28 18:49:31 +0200

    Feat(Database) Add new table

---
+++ Backend/src/main/java/com/example/ztpai/model/FriendRequest.java
@@ -0,0 +1,49 @@
+package com.example.ztpai.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "requests")
+public class FriendRequest {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long senderId;
+
+    @Column(nullable = false)
+    private Long receiverId;
+
+    @Column(nullable = false)
+    private boolean accepted;
+
+
+    public FriendRequest() {}
+
+    public FriendRequest(Long senderId, Long receiverId) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.accepted = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getSenderId() {
+        return senderId;
+    }
+    public void setSenderId(Long senderId) {
+        this.senderId = senderId;
+    }
+    public Long getReceiverId() {
+        return receiverId;
+    }
+    public void setReceiverId(Long receiverId) {
+        this.receiverId = receiverId;
+    }
+}
