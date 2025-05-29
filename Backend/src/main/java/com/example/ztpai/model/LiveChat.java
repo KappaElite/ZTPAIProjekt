30eb0d882f3735d24d97e035901583f4964d60a0
KappaElite <rozkiewiczmilosz@gmail.com>
2025-05-29 12:34:31 +0200

    Feat(Database) Add new database for group chat

---
+++ Backend/src/main/java/com/example/ztpai/model/LiveChat.java
@@ -0,0 +1,56 @@
+package com.example.ztpai.model;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "LiveChat")
+public class LiveChat {
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime sentAt = LocalDateTime.now();
+
+    public LiveChat() {}
+
+    public LiveChat(User sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.sentAt = LocalDateTime.now();
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+}
